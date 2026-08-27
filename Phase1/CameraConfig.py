9af95eeaaad200feb369bc70a842fc6e2bd7c4fb
XanderFerrero <xanderferrero@gmail.com>
2026-08-27 13:56:32 +0800

    Phase 1 IP compatibility

--- Phase1/CameraConfig.py
+++ Phase1/CameraConfig.py
@@ -18,9 +18,10 @@ bufferPolygons = [] # stored mapping spots
 bufferContours = [] # contour states with bufferPolygons
 bufferImage = None # for global access of temporary image
 bufferImageCopy = None # to clear window for editing spot events
-enableIP = False # either real ip cameras or use sample bufferImage
+enableIP = True # either real ip cameras or use sample bufferImage
 quitSpotMaking = False # check if user quits editing parking spots
 deletedPolygon = False # check if user deletes a polygon
+ipCams = dict()
 
 
 #mongodb
@@ -35,16 +36,58 @@ print("Redis Ready")
 
 #load data
 
-data = None
+data = []
 
 
+def keyStr(obj):
+    return f"{obj['location']}-{obj['network']}-{obj['id']}"
 
 def updateData():
     print("Refetching mongodb")
     global data
-    data = [i for i in mdbcol.find()]
+
+    for i in mdbcol.find():
+        data.append(i)
+        ipCams[keyStr(i)] = cv2.VideoCapture(i['url']) #
+
 
 updateData()
+
+def imageCapture(obj):
+    global bufferImage, capWidth, capHeight
+
+    if enableIP:
+
+        ret,bufferImage = ipCams[keyStr(obj)].read()
+        print(ret)
+
+    else:
+        bufferImage = cv2.imread(f"{fileDir}/foo.jpeg")
+
+    if bufferImage is None: return
+
+    capWidth, capHeight, dummy = bufferImage.shape
+
+    bufferImage = cv2.resize(src=bufferImage, dsize=None, fx=(imWidth/max(capWidth, capHeight)), fy=(imWidth/max(capWidth, capHeight)),
+                                      interpolation = cv2.INTER_LINEAR) #resizing
+
+    capWidth, capHeight, dummy = bufferImage.shape
+
+def verifyConnection(obj): 
+    imageCapture(obj)
+
+    if bufferImage is None:
+        print("Please check RTSP/HTTP url. Cannot gather video feed")
+        return
+
+    while True:
+        cv2.imshow("Verify Connection", bufferImage)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27 or key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
 
 def createNewObject():
     while True:
@@ -76,6 +119,7 @@ def createNewObject():
 
     mdbcol.insert_one(newObject)
     updateData()
+    verifyConnection(newObject)
     configureObject(newObject)
 
 def editObject():
@@ -131,7 +175,7 @@ def configureObject(obj): #new set of prompts after selecting/creating object
         choice = input("Enter Choice: ")
 
         if(choice == "1"): #update url
-            newURL = input("Enter New URL")
+            newURL = input("Enter New URL: ")
             mdbcol.find_one_and_update({
                 "location": obj['location'],
                 'id':obj['id'],
@@ -140,6 +184,7 @@ def configureObject(obj): #new set of prompts after selecting/creating object
                 "$set": {"url":newURL}
             })
             updateData()
+            verifyConnection(obj)
         elif(choice == "2"):
             configureSpots(obj)
         elif (choice == "3"):
@@ -150,18 +195,16 @@ def configureObject(obj): #new set of prompts after selecting/creating object
 def configureSpots(obj):
     global bufferPolygons, quitSpotMaking, bufferContours, bufferImage, bufferImageCopy
 
+    imageCapture(obj)
 
-    bufferImage = cv2.imread(f"{fileDir}/foo.jpeg")
-    
+    if bufferImage is None: 
+        print("Please check RTSP/HTTP url. Cannot gather video feed")
+        return
 
-    capWidth, capHeight, dummy = bufferImage.shape
-    bufferImage = cv2.resize(src=bufferImage, dsize=None, fx=(imWidth/max(capWidth, capHeight)), fy=(imWidth/max(capWidth, capHeight)),
-                                      interpolation = cv2.INTER_LINEAR) #resizing
-    capHeight, capWidth,  dummy = bufferImage.shape #update sizes
 
     bufferImageCopy = bufferImage.copy() #get copy to clear window properly 
 
-    bytePolygons = redisClient.get(f"{obj['location']}-{obj['network']}-{obj['id']}-polygons")
+    bytePolygons = redisClient.get(f"{keyStr(obj)}-polygons")
 
     if(bytePolygons == None):
         bufferPolygons = []
@@ -283,6 +326,8 @@ def markSpot():
     bufferPolygon = []
     cv2.destroyAllWindows()
     print(bufferPolygons)
+
+
 
 
 #next tasks
